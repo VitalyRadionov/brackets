d901d5729c64c48fadf77656dfc889b746bb6afc
Radionov_Vitaly <radionovv89@gmail.com>
2022-08-03 03:18:03 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strArray = [...str];
+  let enableSplice = false;
+
+  let res = forEachBracketsConfig(bracketsConfig);
+
+  function forEachBracketsConfig(bConfig) {
+    bConfig.forEach(element => filterArray(strArray, element));
+
+    if (strArray.length > 1 && enableSplice) {
+      enableSplice = false;
+      forEachBracketsConfig(bracketsConfig);
+    }
+    return strArray.length ? false : true;
+  }
+
+  function filterArray(strArray, bConfig) {
+    strArray.filter((element, index, array) => {
+
+      if ((element == bConfig[0]) && (array[index + 1] == bConfig[1])) {
+
+        enableSplice = true;
+        strArray.splice(index, 2);
+
+        if (strArray.length > 1) {
+          filterArray(strArray, bConfig);
+        } else { return true }
+
+        return true;
+
+      } else { return false }
+
+    });
+    return strArray.length ? false : true;
+  }
+
+  return res;
 }
